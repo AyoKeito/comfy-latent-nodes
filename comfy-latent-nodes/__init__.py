ffafee7fe507c5f0c56d5f4a1b375a25a930caf2
Velour-Fog <93813753+Velour-Fog@users.noreply.github.com>
2025-02-21 14:23:39 -0500

    Delete comfy-latent-nodes directory

--- comfy-latent-nodes/__init__.py
+++
@@ -1,9 +0,0 @@
-# Import your custom node classes
-from .custom_save_latent import CustomSaveLatent
-from .custom_load_latent import CustomLoadLatent
-
-# Define NODE_CLASS_MAPPINGS
-NODE_CLASS_MAPPINGS = {
-    "CustomSaveLatent": CustomSaveLatent,
-    "CustomLoadLatent": CustomLoadLatent,
-}
